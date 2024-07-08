37e56954828d6bee91d7b1fb88025edb687fe3cb
yash-dani11 <yashdani11@hotmail.com>
2024-07-08 23:27:58 +0530

    minor UI fixes

--- src/components/VideoContainer/Shimmer.js
+++ src/components/VideoContainer/Shimmer.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Shimmer = () => {
   return (
-    <div className="w-80 sm:ml-5 mb-5">
+    <div className="w-80 sm:ml-1 mb-5">
       <div className="rounded-2xl h-48 w-80 bg-gray-100 animate-blink"></div>
       <div className="px-2 w-80">
         <div className="h-6 bg-gray-100 animate-blink mt-2"></div>
